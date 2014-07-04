585cd72ef527ea25c295b437aed1d721a2c05603
wonje,song <dnjswp95@gmail.com>
2014-07-04 17:33:09 +0900

    3.2.1

--- IgaworksAD.framework/Versions/A/Headers/AdPopcornStyle.h
+++ IgaworksAD.framework/Versions/A/Headers/AdPopcornStyle.h
@@ -17,34 +17,14 @@ typedef NS_ENUM(NSInteger, AdPopcornThemeColor)
     AdPopcornThemeYellowColor = 3
 };
 
-typedef NS_ENUM(NSInteger, AdPopcornTextThemeColor)
-{
-    AdPopcornTextThemeBlueColor = 1,
-    AdPopcornTextThemeRedColor = 2,
-    AdPopcornTextThemeYellowColor = 3
-};
-
-typedef NS_ENUM(NSInteger, AdPopcornRewardThemeColor)
-{
-    AdPopcornRewardThemeBlueColor = 1,
-    AdPopcornRewardThemeRedColor = 2,
-    AdPopcornRewardThemeYellowColor = 3
-};
-
-typedef NS_ENUM(NSInteger, AdPopcornRewardCheckThemeColor)
-{
-    AdPopcornRewardCheckThemeBlueColor = 1,
-    AdPopcornRewardCheckThemeRedColor = 2,
-    AdPopcornRewardCheckThemeYellowColor = 3
-};
 
 
 @interface AdPopcornStyle : NSObject
 
 @property (nonatomic, unsafe_unretained) AdPopcornThemeColor adPopcornThemeColor;
-@property (nonatomic, unsafe_unretained) AdPopcornTextThemeColor adPopcornTextThemeColor;
-@property (nonatomic, unsafe_unretained) AdPopcornRewardThemeColor adPopcornRewardThemeColor;
-@property (nonatomic, unsafe_unretained) AdPopcornRewardCheckThemeColor adPopcornRewardCheckThemeColor;
+@property (nonatomic, unsafe_unretained) AdPopcornThemeColor adPopcornTextThemeColor;
+@property (nonatomic, unsafe_unretained) AdPopcornThemeColor adPopcornRewardThemeColor;
+@property (nonatomic, unsafe_unretained) AdPopcornThemeColor adPopcornRewardCheckThemeColor;
 
 @property (nonatomic, unsafe_unretained) UIColor *adPopcornCustomThemeColor;
 @property (nonatomic, unsafe_unretained) UIColor *adPopcornCustomTextThemeColor;
