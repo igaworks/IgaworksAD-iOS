69ee64d2163e21cfe0b397ac4367667d8ab060e9
wonje,song <dnjswp95@gmail.com>
2014-08-20 11:39:34 +0900

    4.0.0

---
+++ IgaworksAD.framework/Versions/A/Headers/DAPopupAd.h
@@ -0,0 +1,39 @@
+//
+//  DAPopupAd.h
+//  IgaworksAd
+//
+//  Created by wonje,song on 2014. 4. 22..
+//  Copyright (c) 2014년 wonje,song. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import "DAError.h"
+#import "IgaworksAD.h"
+
+
+@protocol DAPopupAdDelegate;
+
+@interface DAPopupAd : NSObject
+
+
+@property (nonatomic, unsafe_unretained) id<DAPopupAdDelegate> delegate;
+
+
+- (id)initWithKey:(NSString *)mediaKey spotKey:(NSString *)spotKey;
+- (BOOL)presentFromViewController:(UIViewController *)viewController;
+- (void)setLogLevel:(IgaworksADLogLevel)logLevel;
+
+@end
+
+@protocol DAPopupAdDelegate <NSObject>
+
+@optional
+- (void)DAPopupAdDidLoad:(DAPopupAd *)popupAd;
+
+- (void)DAPopupAd:(DAPopupAd *)popupAd didFailToReceivetialAdWithError:(DAError *)error;
+
+- (void)DAPopupAdWillLeaveApplication:(DAPopupAd *)popupAd;
+
+@end
